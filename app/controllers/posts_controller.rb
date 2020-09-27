2bf29da2382bd0d2fe75ddc6bbd5a67f6bfbf809
naotomineta <71373810+naotomineta@users.noreply.github.com>
2020-09-27 15:17:20 +0900

    h

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
 
   def checked
     post = Post.find(params[:id])
-    if @post.checked
+    if post.checked then
       post.update(checked: false)
     else
       post.update(checked: true)
@@ -20,4 +20,5 @@ class PostsController < ApplicationController
     item = Post.find(params[:id])
     render json: {post:item}
   end
+
 end
